fe05794e0c719dc9e0af5514a14a2dc9ad02221f
xeeyyal <tu5luzvdh@code.edu.az>
2023-10-10 00:31:12 +0400

    task10.10 Strin,StringBuilder,Char,Array

---
+++ task10.10/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Globalization;
+using System.Text;
+
+namespace task10._10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //TASK-1
+
+            string sentence = "   Xeyal huseynoV gRouP ab104,   Technical uniVersitY";
+
+            string changedSentence = ChangeSentence(sentence);
+
+            Console.WriteLine(changedSentence);
+        }
+        static string ChangeSentence(string sentence)
+        {
+            if (string.IsNullOrWhiteSpace(sentence))
+                return string.Empty;
+
+            string[] words = sentence.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            StringBuilder result = new StringBuilder();
+
+            foreach (string word in words)
+            {
+                if (result.Length > 0)
+                {
+                    result.Append(' ');
+                }
+
+                string changedWord = char.ToUpper(word[0]) + word.Substring(1).ToLower();
+                result.Append(changedWord);
+            }
+
+            return result.ToString();
+        }
+    }
+}
